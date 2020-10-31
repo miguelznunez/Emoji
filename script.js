a8209153db7ad3058dd2be4e7b0b1fe74068dacd
iamcodefoxx <71807189+iamcodefoxx@users.noreply.github.com>
2020-10-31 12:49:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const box = document.getElementById("box");
+const text = document.getElementById("text");
+t = text.innerHTML;
+const emoji = document.getElementById("emoji");
+var e = document.getElementById("face");
+
+box.addEventListener("focusin", function(){
+  text.innerHTML = "Enter number 1 - 5";
+});
+
+box.addEventListener("focusout", function(){
+  text.innerHTML = t;
+  emoji.innerHTML = "";
+})
+
+box.addEventListener("keydown", function (event){
+  text.innerHTML = "";
+  switch(event.code){
+    case "Digit1":
+      emoji.innerHTML = '&#128545;';
+      break;
+    case "Digit2":
+      emoji.innerHTML = '&#128577;';
+      break;
+    case "Digit3":
+      emoji.innerHTML = '&#128528;';
+      break;
+    case "Digit4":
+      emoji.innerHTML = '&#128578;';
+      break;
+    case "Digit5":
+      emoji.innerHTML = '&#128513;';
+      break;
+  }
+});
+
+  
